b8cb65b2e7e28745f04694acb24dc362b4337b19
Peter <schmidt.multimedia@gmail.com>
2021-11-18 13:51:14 +0100

    Beispiel birthYear

--- script_01.js
+++ script_01.js
@@ -13,6 +13,7 @@
 // console.log(firstName); // nicht deklarierte Var. --> Fehler
 
 /***** 02 Deklaration + Wertzuweisung I *******/
+
 // let firstName; // Deklaration (Definition)
 // firstName = "Max"; // Wertzuweisung
 // console.log(firstName); // Ausgabe
@@ -22,6 +23,7 @@
 // console.log(firstName + " " + familyName);
 
 /***** 03 Deklaration + Wertzuweisung II *******/
+
 // let firstName, familyName;
 // firstName = prompt("Bitte Vornamen eingeben:");
 // familyName = prompt("Bitte Nachnamen eingeben:");
@@ -29,12 +31,36 @@
 
 // JS ist eine untypisierte Sprache! | untyped
 
-let test;
-test = "hi";
-test = 2;
-test = true;
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
+// let test;
+// test = "hi";
+// test = 2;
+// test = true;
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
 
+/***** 03a Konstanten *******/
+// const test = "hi"; // Variable mit KONSTANTEM Inhalt
+// test = "hallo"; // KEINE neue Zuweisung zur LZ möglich!
+// console.log("Inhalt: " + test);
 
+/***** Beispiel *******/
 
+// Deklaration
+let ageJohn, ageMark;
+let birthYearJohn, birthYearMark;
+
+// let year = 2021;
+let date = new Date();
+let year = date.getFullYear();
+
+// Wertzuweisung
+ageJohn = 25;
+ageMark = 30;
+
+// Berechnung
+birthYearJohn = year - ageJohn;
+birthYearMark = year - ageMark;
+
+// Ausgabe
+console.log("birthYearJohn: " + birthYearJohn);
+console.log("birthYearMark: " + birthYearMark);
