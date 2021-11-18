a4d2ba7c2c3de88dba74594b029c9952102ae35d
Peter <schmidt.multimedia@gmail.com>
2021-11-18 11:17:04 +0100

    Deklaration / Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -1,2 +1,36 @@
 /***** Variablen 01 *******/
 
+/**
+ * 
+ *  Block-Kommentar
+ * 
+ */
+
+// Zeilen-Kommentar
+
+// Konsolen-Ausgabe
+// console.log("Hello, World!");
+// console.log(firstName); // nicht deklarierte Var. --> Fehler
+
+/***** 02 Deklaration + Wertzuweisung I *******/
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log(familyName); // Ausgabe
+// console.log(firstName + " " + familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+// let firstName, familyName;
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+// console.log(firstName + " " + familyName);s
+
+let test;
+test = "hi";
+test = 2;
+test = true;
+console.log("Inhalt: " + test);
+
+
